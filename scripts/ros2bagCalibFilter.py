1d05551b58d9ca6f1df08654e2929e0842928d8f
cesardebeunne <debeunnecesar@gmail.com>
2024-06-25 17:31:49 +0200

    add argument parsing

--- scripts/ros2bagCalibFilter.py
+++ scripts/ros2bagCalibFilter.py
@@ -1,7 +1,6 @@
 import cv2
-import pandas as pd
+import argparse
 import os
-
 
 import rosbag2_py
 from rclpy.serialization import serialize_message
@@ -12,30 +11,20 @@ from sensor_msgs.msg import Image
 from sensor_msgs.msg import Imu
 from sensor_msgs.msg import CameraInfo
 
-pd.set_option('display.float_format', str)
-
-
-def get_rosbag_options(path, storage_id, serialization_format='cdr'):
-    storage_options = rosbag2_py.StorageOptions(
-        uri=path, storage_id=storage_id)
-
-    converter_options = rosbag2_py.ConverterOptions(
-        input_serialization_format=serialization_format,
-        output_serialization_format=serialization_format)
-
-    return storage_options, converter_options
 
 if __name__ == '__main__':
-
-    bag_path = '/media/ce.debeunne/HDD/datasets/ISAE/2024-03-18-Cagnac/ros2bag/Radar2_light_run'
-    storage_id = 'sqlite3'
-    storage_options, converter_options = get_rosbag_options(bag_path, storage_id)
-
-    # Set filter for topic of string type
-    storage_filter = rosbag2_py.StorageFilter(topics=['/cam_0/image_raw', '/cortex/ouster/points'])
+    
+    # Parse arguments
+    parser = argparse.ArgumentParser(description='Bag formatting')
+    parser.add_argument('--bag_path', type=str, required=True, help="Path of the ROS2 bag")
+    parser.add_argument('--camera_topic', type=str, required=True, help="Topic of the image")
+    parser.add_argument('--lidar_topic', type=str, required=True, help="Topic of the LiDAR")
+    parser.add_argument('--stop_stamp', type=int, required=False, help="Stamp to stop bag processing", default=2000000000000000000000)
+    args = parser.parse_args()
+    bag_path = args.bag_path
     
     # ROS2 bag writer
-    os.system('rm -r /media/ce.debeunne/HDD/datasets/ISAE/2024-03-18-Cagnac/ros2bag/Radar2_light_run_filtered')
+    os.system(f'rm -r {bag_path}_filtered')
     writer = rosbag2_py.SequentialWriter()
     storage_options = rosbag2_py._storage.StorageOptions(
             uri=bag_path+'_filtered',
@@ -45,17 +34,17 @@ if __name__ == '__main__':
     
     # Create topics
     topic_info = rosbag2_py._storage.TopicMetadata(
-            name='/cam_0/image_raw',
+            name=args.camera_topic,
             type='sensor_msgs/msg/Image',
             serialization_format='cdr')
     writer.create_topic(topic_info)
     topic_info = rosbag2_py._storage.TopicMetadata(
-            name='/cam_0/camera_info',
+            name='/cam/camera_info',
             type='sensor_msgs/msg/CameraInfo',
             serialization_format='cdr')
     writer.create_topic(topic_info)
     topic_info = rosbag2_py._storage.TopicMetadata(
-            name='/cortex/ouster/points',
+            name=args.lidar_topic,
             type='sensor_msgs/msg/PointCloud2',
             serialization_format='cdr')
     writer.create_topic(topic_info)
@@ -64,11 +53,11 @@ if __name__ == '__main__':
         for conn, timestamp, data in reader.messages():
             msg = deserialize_cdr(data, conn.msgtype)
             
-            if (timestamp > 1710776357888979114):
+            if (timestamp > args.stop_stamp):
             	break
             
-            if (conn.topic == "/cam_0/image_raw"):
-                writer.write('/cam_0/image_raw', data, timestamp)
+            if (conn.topic == args.camera_topic,):
+                writer.write(args.camera_topic, data, timestamp)
                 
                 cam_info_msg = CameraInfo()
                 cam_info_msg.header.stamp.sec = msg.header.stamp.sec
@@ -78,9 +67,9 @@ if __name__ == '__main__':
                 cam_info_msg.distortion_model = "equidistant"
                 cam_info_msg.d = [0.01879377, -0.00132283, -0.00234174, 0.00032518]
                 cam_info_msg.k = [499.86967694, 0.0, 650.07989234, 0.0, 499.77443338, 531.47666637, 0.0, 0.0, 1.0]
-                writer.write('/cam_0/camera_info', serialize_message(cam_info_msg), timestamp)
+                writer.write('/cam/camera_info', serialize_message(cam_info_msg), timestamp)
                 print(cam_info_msg)
                 
-            if (conn.topic == "/cortex/ouster/points"):
-                writer.write('/cortex/ouster/points', data, timestamp)
+            if (conn.topic == args.lidar_topic):
+                writer.write(args.lidar_topic, data, timestamp)
                 
