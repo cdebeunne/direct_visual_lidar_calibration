70c03f508ae2f3a77b494904dbb81ba393b73c3d
cesardebeunne <debeunnecesar@gmail.com>
2024-06-26 16:28:59 +0200

    add yaml config file

--- scripts/ros2bagCalibFilter.py
+++ scripts/ros2bagCalibFilter.py
@@ -1,5 +1,6 @@
 import cv2
 import argparse
+import yaml
 import os
 
 import rosbag2_py
@@ -17,11 +18,19 @@ if __name__ == '__main__':
     # Parse arguments
     parser = argparse.ArgumentParser(description='Bag formatting')
     parser.add_argument('--bag_path', type=str, required=True, help="Path of the ROS2 bag")
-    parser.add_argument('--camera_topic', type=str, required=True, help="Topic of the image")
-    parser.add_argument('--lidar_topic', type=str, required=True, help="Topic of the LiDAR")
-    parser.add_argument('--stop_stamp', type=int, required=False, help="Stamp to stop bag processing", default=2000000000000000000000)
+    parser.add_argument('--config_path', type=str, required=True, help="Path of the yaml config file")
     args = parser.parse_args()
     bag_path = args.bag_path
+    with open(args.config_path, 'r') as file:
+        config = yaml.load(file)
+    camera_topic = config['camera_topic']
+    lidar_topic = config['lidar_topic']
+    stop_stamp = config['stop_stamp']
+    height = config["height"]
+    width = config["width"]
+    distortion_model = config["distortion_model"]
+    distortion_coefficients = config["distortion_coefficients"]
+    intrinsics = config["intrinsics"]
     
     # ROS2 bag writer
     os.system(f'rm -r {bag_path}_filtered')
@@ -34,7 +43,7 @@ if __name__ == '__main__':
     
     # Create topics
     topic_info = rosbag2_py._storage.TopicMetadata(
-            name=args.camera_topic,
+            name=camera_topic,
             type='sensor_msgs/msg/Image',
             serialization_format='cdr')
     writer.create_topic(topic_info)
@@ -44,7 +53,7 @@ if __name__ == '__main__':
             serialization_format='cdr')
     writer.create_topic(topic_info)
     topic_info = rosbag2_py._storage.TopicMetadata(
-            name=args.lidar_topic,
+            name=lidar_topic,
             type='sensor_msgs/msg/PointCloud2',
             serialization_format='cdr')
     writer.create_topic(topic_info)
@@ -53,23 +62,23 @@ if __name__ == '__main__':
         for conn, timestamp, data in reader.messages():
             msg = deserialize_cdr(data, conn.msgtype)
             
-            if (timestamp > args.stop_stamp):
+            if (stop_stamp != 0 & timestamp > stop_stamp):
             	break
             
-            if (conn.topic == args.camera_topic,):
-                writer.write(args.camera_topic, data, timestamp)
+            if (conn.topic == camera_topic,):
+                writer.write(camera_topic, data, timestamp)
                 
                 cam_info_msg = CameraInfo()
                 cam_info_msg.header.stamp.sec = msg.header.stamp.sec
                 cam_info_msg.header.stamp.nanosec = msg.header.stamp.nanosec
-                cam_info_msg.height = 1024
-                cam_info_msg.width = 1280
-                cam_info_msg.distortion_model = "equidistant"
-                cam_info_msg.d = [0.01879377, -0.00132283, -0.00234174, 0.00032518]
-                cam_info_msg.k = [499.86967694, 0.0, 650.07989234, 0.0, 499.77443338, 531.47666637, 0.0, 0.0, 1.0]
+                cam_info_msg.height = height
+                cam_info_msg.width = width
+                cam_info_msg.distortion_model = distortion_model
+                cam_info_msg.d = distortion_coefficients
+                cam_info_msg.k = intrinsics
                 writer.write('/cam/camera_info', serialize_message(cam_info_msg), timestamp)
                 print(cam_info_msg)
                 
-            if (conn.topic == args.lidar_topic):
-                writer.write(args.lidar_topic, data, timestamp)
+            if (conn.topic == lidar_topic):
+                writer.write(lidar_topic, data, timestamp)
                 
